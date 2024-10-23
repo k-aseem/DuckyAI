cb29b2833348a2d6ff57146ae75f0cc94cc1251c
Aseem Khandelwal <123151016+k-aseem@users.noreply.github.com>
2024-10-23 11:17:51 -0400

    module6 files

---
+++ Module6/ducky-ui/helpers/sidebar.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def show() -> None:
+	with st.sidebar:
+		st.markdown(f"""
+			<a href="/" style="color:yellow;text-decoration: none;">
+				<div style="display:table;margin-top:-16rem;margin-left:0%;">
+					<img src="/app/static/logo.png" width="30"><span> Ducky</span>
+					<span style="font-size: 0.8em; color: grey">&nbsp;&nbsp;v0.1.0</span>
+				</div>
+			</a>
+			<br>
+				""", unsafe_allow_html=True)
+
+		reload_button = st.button("↪︎  Reload Page")
+		if reload_button:
+			st.session_state.clear()
+			st.rerun()
+
